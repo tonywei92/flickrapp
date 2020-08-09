2275b7670e24389c89be666161929b13b4dc64e4
Tony Song <tonywei92@gmail.com>
2020-08-10 00:46:46 +0700

    test: add integration test for flicker feeds refresh button

--- cypress/integration/App.e2e.js
+++ cypress/integration/App.e2e.js
@@ -10,4 +10,12 @@ context('Flickr Public Feeds Integration Test', () => {
     cy.wait('@getFlickrFeeds').its('status').should('be', 200);
     cy.get('.swiper-slide').its('length').should('eq', 20);
   });
+
+  it('Should refresh and load 20 flickr feeds', () => {
+    cy.wait('@getFlickrFeeds').its('status').should('be', 200);
+    cy.get('.swiper-slide').its('length').should('eq', 20);
+    cy.get('#refresh-feeds').click();
+    cy.wait('@getFlickrFeeds').its('status').should('be', 200);
+    cy.get('.swiper-slide').its('length').should('eq', 20);
+  });
 });

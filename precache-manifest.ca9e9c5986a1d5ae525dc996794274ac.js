91b5e5e471378d9d3bfdf012e6c6f0fd521793e6
Tony Song <tonywei92@gmail.com>
2020-08-10 01:03:24 +0700

    Updates

---
+++ precache-manifest.ca9e9c5986a1d5ae525dc996794274ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c142c16fc0d58900b32896e604c1274f",
+    "url": "/flickrapp/index.html"
+  },
+  {
+    "revision": "03f6a775c6c9838c5f6f",
+    "url": "/flickrapp/static/css/2.a1063ac4.chunk.css"
+  },
+  {
+    "revision": "dd3d54b229e525a7f204",
+    "url": "/flickrapp/static/css/main.b72d524d.chunk.css"
+  },
+  {
+    "revision": "03f6a775c6c9838c5f6f",
+    "url": "/flickrapp/static/js/2.4c8af5e9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/flickrapp/static/js/2.4c8af5e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd3d54b229e525a7f204",
+    "url": "/flickrapp/static/js/main.f513341f.chunk.js"
+  },
+  {
+    "revision": "c99b74985a9e79238832",
+    "url": "/flickrapp/static/js/runtime-main.a43c84e3.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/flickrapp/precache-manifest.ca9e9c5986a1d5ae525dc996794274ac.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/flickrapp/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
